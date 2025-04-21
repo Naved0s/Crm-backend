a5e2eb80f51f659a0e993c663a06cb6c19557452
Naved0s <shaikhnaved2003@outlook.com>
2025-04-21 18:27:25 +0530

    LeadFinished

--- src/main/java/com/crm/CrmSystem/controller/LeadController.java
+++ src/main/java/com/crm/CrmSystem/controller/LeadController.java
@@ -1,32 +1,48 @@
 package com.crm.CrmSystem.controller;
 
-import com.crm.CrmSystem.models.LeadSource;
-import com.crm.CrmSystem.services.LeadSourceService;
+import com.crm.CrmSystem.models.Lead;
+import com.crm.CrmSystem.repository.LeadRepository;
+import com.crm.CrmSystem.services.LeadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/leadsource")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/lead")
+@CrossOrigin(origins = "*")
 public class LeadController {
 
     @Autowired
-    LeadSourceService leadSourceService;
+    LeadService leadService;
 
-    //GET ALL LEADSOURCE
-    @GetMapping("/all")
-    public List<LeadSource> getAll(){
-        System.out.println("Getting all"    );
-    return leadSourceService.getAllLeadSources();
-    }
-
-    //Add leadSource
     @PostMapping("/addlead")
-    public LeadSource addLead(@RequestBody LeadSource leadSource){
-        System.out.println("added : "+ leadSource);
-        return leadSourceService.saveLeadSource(leadSource);
+    public Lead addlead(@RequestBody Lead lead){
+     //   lead.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+        return leadService.addlead(lead);
     }
+
+    @GetMapping("/getall")
+    public List<Lead> getall(){
+        return leadService.getAllLead();
+    }
+
+//    @PutMapping("/update")
+//    public void updateLead(@RequestParam String leadStatus, @RequestParam int id){
+//        leadService.updateLead(leadStatus,id);
+//    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateLead(@PathVariable int id, @RequestBody String leadStatus) {
+
+            leadService.updateLead(id, leadStatus);
+            System.out.println("This is latest status:"+leadStatus);
+            return ResponseEntity.ok("Updated lead status to " + leadStatus);
+
+    }
+
 
 }
